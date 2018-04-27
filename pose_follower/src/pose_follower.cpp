BadgerTechnologies/navigation_experimental

commit cded2ef98ada48aa618776cac4b80a74bc881e42

pose_follower: Prevent NAN velocity output
Under certain edge cases, it can output NAN as the x linear velocity command.
This causes the Gazebo simulation's differential drive plugin to crash.

pose_follower/src/pose_follower.cpp
@@ -327,6 +327,10 @@ namespace pose_follower {
 
     if (fabs(res.angular.z) > max_vel_th_) res.angular.z = max_vel_th_ * sign(res.angular.z);
     if (fabs(res.angular.z) < min_vel_th_) res.angular.z = min_vel_th_ * sign(res.angular.z);
+    if (std::isnan(res.linear.x))
+        res.linear.x = 0.0;
+    if (std::isnan(res.linear.y))
+        res.linear.x = 0.0;
 
     //we want to check for whether or not we're desired to rotate in place
     if(sqrt(twist.linear.x * twist.linear.x + twist.linear.y * twist.linear.y) < in_place_trans_vel_){

commit b2a5ba9445b77e31f97af0c3e9cd7939061829ed

pose_follower: Fix typo in preventing NaN for Y
Dumb copy/paste error.

Fixes: cded2ef ("pose_follower: Prevent NAN velocity output")

pose_follower/src/pose_follower.cpp
@@ -330,7 +330,7 @@ namespace pose_follower {
     if (std::isnan(res.linear.x))
         res.linear.x = 0.0;
     if (std::isnan(res.linear.y))
-        res.linear.x = 0.0;
+        res.linear.y = 0.0;
 
     //we want to check for whether or not we're desired to rotate in place
     if(sqrt(twist.linear.x * twist.linear.x + twist.linear.y * twist.linear.y) < in_place_trans_vel_){